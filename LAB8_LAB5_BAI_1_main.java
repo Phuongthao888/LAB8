f386a71b7062bc9dd0ca4954a709722a7025a788
Phương Thảo <124881167+Phuongthao888@users.noreply.github.com>
2023-04-12 21:59:46 +0700

    Create LAB8_LAB5_BAI_1_main.java

---
+++ LAB8_LAB5_BAI_1_main.java
@@ -0,0 +1,11 @@
+package LAB5_BAI1;
+
+public class main {
+    public static void main(String[] args) {
+        DanhSachSoThuc danhsach = new DanhSachSoThuc();
+        
+        danhsach.Nhap();
+        System.out.println("\nDanh sach so thuc duoc nhap: ");
+        danhsach.Xuat();
+    }
+}
