3722cf3a2cd59e48fd45129738a3b4b0aea82855
Phương Thảo <124881167+Phuongthao888@users.noreply.github.com>
2023-04-12 21:57:43 +0700

    Create LAB8_lab5_BAI_1_DOUBLE.java

---
+++ LAB8_lab5_BAI_1_DOUBLE.java
@@ -0,0 +1,18 @@
+package LAB5_BAI1;
+
+import java.util.ArrayList;
+
+public class Double {
+    public float x;
+    
+    public Double(){
+     }
+     
+    public Double(float x){
+        this.x = x;
+    }
+    
+    public String toString(){
+        return "    " + x;
+    }
+}
