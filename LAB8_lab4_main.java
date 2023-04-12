585f59efb8ded81bf68db1d2fff3132ee615beb2
Phương Thảo <124881167+Phuongthao888@users.noreply.github.com>
2023-04-12 14:06:28 +0700

    Create LAB8_lab4_main.java

---
+++ LAB8_lab4_main.java
@@ -0,0 +1,11 @@
+package com.mycompany.lab8;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        SanPham sp = new SanPham();
+        sp.Nhap();
+        sp.Xuat();
+    }
+}
